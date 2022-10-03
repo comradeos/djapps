510160397cafcef47f1d3eb6dc2cedce20b512e1
Iaroslav Os <osmnbx@gmail.com>
2022-10-03 23:07:45 +0300

    added/updated

--- sources/djapps/tomato/models.py
+++ sources/djapps/tomato/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
+class SalesOrder(models.Model):
+    amount = models.IntegerField()
+    description = models.CharField(max_length=255)
