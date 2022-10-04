71936dfc5fafaa1e160fc25b4bcce2d45328dddb
Iaroslav Os <osmnbx@gmail.com>
2022-10-04 22:21:42 +0300

    added/updated

--- sources/djapps/tomato/models.py
+++ sources/djapps/tomato/models.py
@@ -7,9 +7,4 @@ from datetime import datetime
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
-    date = models.DateTimeField(default=datetime.now())
-    email = models.EmailField(default='test@email.com')
-    bool = models.BooleanField(default=1)
-    float = models.FloatField(default=0.0)
-    time = models.TimeField(default=datetime.now())
-    text = models.TextField(null=True)
+
