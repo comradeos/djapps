367dc688d5715f76627eb53865b81fd47e03ef03
Iaroslav Os <osmnbx@gmail.com>
2022-10-05 21:07:35 +0300

    added/updated

--- sources/djapps/tomato_orders/models.py
+++ sources/djapps/tomato_orders/models.py
@@ -4,9 +4,16 @@ from django.db import models
 from tomato_products.models import Product
 
 
+class Additional(models.Model):
+    title = models.CharField(max_length=255)
+
+
 # Create your models here.
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     products = models.ManyToManyField(Product)
+    test = models.OneToOneField(Additional, on_delete=models.CASCADE, null=True)
+
+
