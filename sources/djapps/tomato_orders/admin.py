ceaa767fb73551558ae76d797281553bf9ecfee8
Iaroslav Os <osmnbx@gmail.com>
2022-10-05 21:11:53 +0300

    added/updated

--- sources/djapps/tomato_orders/admin.py
+++ sources/djapps/tomato_orders/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import SalesOrder
+from .models import SalesOrder, Additional
 
 
 # Register your models here.
 admin.site.register(SalesOrder)
+admin.site.register(Additional)
